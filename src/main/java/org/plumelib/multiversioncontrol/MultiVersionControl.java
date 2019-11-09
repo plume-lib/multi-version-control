539fe2a152ed64aa7faf3f2b7e8ce27cb4c19a5c
Michael Ernst <mernst@cs.washington.edu>
2019-11-09 07:33:45 -0800

    Do replacements even in long output

--- src/main/java/org/plumelib/multiversioncontrol/MultiVersionControl.java
+++ src/main/java/org/plumelib/multiversioncontrol/MultiVersionControl.java
@@ -1253,12 +1253,47 @@ public class MultiVersionControl {
      */
     public String replaceAll(String s) {
       // String.replaceAll uses a recursive (!) algorithm that is prone to StackOverflowError for
-      // long strings.
-      if (s.length() > 10000) {
-        return s;
-      } else {
+      // long strings.  This recursion should be OK because it's much less deep.
+      int lineStart = lineStartIndex(s, 10000);
+      if (lineStart == -1) {
         return s.replaceAll(regexp, replacement);
+      } else {
+        String firstPart = s.substring(0, lineStart);
+        String lastPart = s.substring(lineStart);
+        return firstPart.replaceAll(regexp, replacement) + replaceAll(lastPart);
       }
+    }
+  }
+
+  /**
+   * Given a string, return the index of the start of a line, after {@code start}.
+   *
+   * @param start the index at which to start looking for the start of a line
+   * @return the index of the start of a line, or -1 if no such exists
+   */
+  private static @IndexOrLow("#1") int lineStartIndex(String s, int start) {
+    if (s.length() == 0) {
+      return -1;
+    }
+    if (start == 0) {
+      // It doesn't make sense to call this routine with 0, but return 0 anyway.
+      return 0;
+    }
+    if (start > s.length()) {
+      return -1;
+    }
+    // possible line terminators:  "\n", "\r\n", "\r".
+    int newlinePos = s.indexOf("\n", start - 1);
+    int afterNewline = (newlinePos == -1) ? Integer.MAX_VALUE : newlinePos + 1;
+    int returnPos1 = s.indexOf("\r\n", start - 2);
+    int returnPos2 = s.indexOf("\r", start - 1);
+    int afterReturn1 = (returnPos1 == -1) ? Integer.MAX_VALUE : returnPos1 + 2;
+    int afterReturn2 = (returnPos2 == -1) ? Integer.MAX_VALUE : returnPos2 + 1;
+    int lineStart = Math.min(afterNewline, Math.min(afterReturn1, afterReturn2));
+    if (lineStart >= s.length()) {
+      return -1;
+    } else {
+      return lineStart;
     }
   }
 
